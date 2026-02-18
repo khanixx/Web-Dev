50a45bb890f91096a75123e748071f48ae53c9c9
khanixx <kamalievabylaikhan@gmail.com>
2026-02-18 19:04:51 +0500

    Lab4

---
+++ Lab3/Task1/script.js
@@ -0,0 +1,153 @@
+//alert('hello, world!');
+
+//alert('Привет'); alert('Мир');
+
+//alert(3 +
+// 1
+// + 2);
+
+//alert('Hello');
+//
+// [1, 2].forEach(alert);
+
+//"use strict";
+
+// let message;
+//
+// message = "Hello World!";
+// alert(message);
+
+//let user = 'John', age = 25, message = 'Hello';
+
+//let user = 'John';
+// let age = 25;
+// let message = 'Hello';
+
+//task1
+// let admin;
+// let name = "John";
+// admin = name;
+//
+// alert(admin);
+
+//task2
+// let planetUniverse = "Earth";
+// let currentUser;
+
+// let age = prompt('Сколько тебе лет?');
+//
+// alert(`Тебе ${age} лет!`);
+
+// let value = true;
+// alert(typeof value); // boolean
+//
+// value = String(value); // теперь value это строка "true"
+// alert(typeof value);
+
+//alert('4' - '1');
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+//
+// alert(+a + +b);
+
+// true
+// false
+// true
+// true
+// false
+// false
+// false
+
+// if ("") {
+//     alert( 'Привет' );
+// }
+
+// let company = prompt("какое официальное название джаваскрипт");
+//
+// (company == 'Javascript') ? alert('correct') : alert('failed');
+
+// let user = prompt("number");
+//
+// let message;
+//
+// if(user > 0) {
+//     message = 1;
+// } else if(user < 0) {
+//     message = -1;
+// } else {
+//     message = 0;
+// }
+//
+// alert(message);
+
+// let userName = prompt("Кто там?", '');
+//
+// if (userName === 'Админ') {
+//
+//     let pass = prompt('Пароль?', '');
+//
+//     if (pass === 'Я главный') {
+//         alert( 'Здравствуйте!' );
+//     } else if (pass === '' || pass === null) {
+//         alert( 'Отменено' );
+//     } else {
+//         alert( 'Неверный пароль' );
+//     }
+//
+// } else if (userName === '' || userName === null) {
+//     alert( 'Отменено' );
+// } else {
+//     alert( "Я вас не знаю" );
+// }
+
+// let num1 = 10,
+//     num2 = 20,
+//     result;
+//
+// result ??= num1 ?? num2;
+
+// for(let i = 2; i < 11; i++){
+//     if(i % 2 == 0){
+//         alert(i)
+//     }
+// }
+
+// let i = 0;
+// while (i < 3) {
+//     alert( `number ${i}!` );
+//     i++;
+// }
+
+// let n = 10;
+//
+// label:
+// for(let i = 2; i < n; i++) {
+//
+//     for(let j = 2; j < i; j++) {
+//         if(i % j == 0) {
+//             continue label;
+//         }
+//     }
+//     alert(i);
+// }
+
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Родители разрешили?');
+// }
+
+// let sum = function(a, b) {
+//     return a + b;
+// };
+//
+// alert(sum(1, 2));
+
+// let user = {
+//     name: "John",
+//     age: 30
+// };
+//
+// let key = prompt("Что вы хотите узнать о пользователе?", "name");
+//
+// alert( user[key] );
+
